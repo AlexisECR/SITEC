2b830e7ada7b2ef912b1931aec8c6ebb7d1a86e9
alexis.eduardo07 <alexis.eduardo07@gmail.com>
2025-05-29 13:24:28 -0700

    Actualizacion Valida-Formulario

--- assets/js/pages/agregar.js
+++ assets/js/pages/agregar.js
@@ -2,67 +2,77 @@ document.getElementById("main-content").addEventListener("click", (e) => {
     if (e.target.id === "btn-guardar") {
         e.preventDefault();
 
+        let registroValido = true;
+
         const camposAValidar = [
-            { campoId: "nombre", errorId: "error-nombre", mensaje: "El campo nombre es obligatorio." },
-            { campoId: "correo", errorId: "error-correo", mensaje: "El campo correo es obligatorio." },
-            { campoId: "telefono", errorId: "error-telefono", mensaje: "El campo teléfono es obligatorio." },
-            { campoId: "departamento", errorId: "error-departamento", mensaje: "El campo departamento es obligatorio." },
-            { campoId: "fecha", errorId: "error-fecha", mensaje: "El campo fecha es obligatorio." },
-            { campoId: "descripcion", errorId: "error-descripcion", mensaje: "El campo descripción es obligatorio." }
+            {campoId: "nombre", errorId: "error-nombre", mensaje: "El campo nombre es obligatorio."},
+            {campoId: "correo", errorId: "error-correo", mensaje: "El campo correo es obligatorio."},
+            {campoId: "telefono", errorId: "error-telefono", mensaje: "El campo teléfono es obligatorio."},
+            {campoId: "departamento", errorId: "error-departamento", mensaje: "El campo departamento es obligatorio."},
+            {campoId: "fecha", errorId: "error-fecha", mensaje: "El campo fecha es obligatorio."},
+            {campoId: "descripcion", errorId: "error-descripcion", mensaje: "El campo descripción es obligatorio."}
         ];
 
         const tipo_incidente = document.querySelector('input[name="tipo_incidente"]:checked');
         const errorIncidente = document.getElementById("error-incidente");
 
         const equipo = Array.from(document.querySelectorAll('input[name="equipo[]"]:checked'));
-        const errorEquipo = document.getElementById("error-checkbox");
+        const errorEquipo = document.getElementById("errorCheckbox");
 
-        camposAValidar.forEach(({ campoId, errorId, mensaje }) => {
+        camposAValidar.forEach(({campoId, errorId, mensaje}) => {
             const campo = document.getElementById(campoId);
             const error = document.getElementById(errorId);
-            validaInput(campo, error, mensaje);
+            if(!validaInput(campo, error, mensaje)){
+                registroValido = false;
+            }
         });
+        if (!validaRadioButton(tipo_incidente, errorIncidente)){
+            registroValido = false;
+        }
 
-        console.log(camposAValidar.forEach(campoId)f);
+        if (!validaCheckbox(equipo, errorEquipo)) {
+            registroValido = false;
+        }
 
+        imprimirJson(registroValido);
+    }
+});
 
-        /*  if (correo.value.trim() === "") {
-              errorCorreo.textContent = "El campo correo es obligatorio.";
-              correo.classList.add("input-error");
-          }
-          if (telefono.value.trim() === "") {
-              errorTelefono.textContent = "El campo telefono es obligatorio.";
-              telefono.classList.add("input-error");
-          }
+function validaInput(campo, error, mensaje) {
+    if (campo.value.trim() === "") {
+        error.textContent = mensaje;
+        campo.classList.add("input-error");
+        return false;
+    } else {
+        error.textContent = "";
+        campo.classList.remove("input-error");
+        return true;
+    }
+}
 
-          if (!tipo_incidente) {
-              errorIncidente.textContent = "Debe seleccionar un tipo de incidente.";
-          }
+function validaRadioButton(campo, error) {
+    if (!campo) {
+        error.textContent = "Debe seleccionar un tipo de incidente.";
+        return false;
+    } else {
+        error.textContent = "";
+        return true;
+    }
+}
 
-          if (departamento.value === "") {
-              errorDepartamento.textContent = "Seleccione un departamento.";
-              departamento.classList.add("input-error");
-          }
+function validaCheckbox(campo, error) {
+    if (campo.length === 0) {
+        error.textContent = "Debe seleccionar al menos un equipo.";
+        return false;
+    } else {
+        error.textContent = "";
+        return true;
+    }
+}
 
-          if (fecha.value === "") {
-              errorFecha.textContent = "Fecha es obligatorio.";
-              fecha.classList.add("input-error");
-          }
-
-          if (equipo.length === 0) {
-              errorEquipo.textContent = "Equipo es obligatorio.";
-              fecha.classList.add("input-error");
-          }
-
-          if (descripcion.value === "") {
-              errorDescripcion.textContent = "Descripcion es obligatorio.";
-              descripcion.classList.add("input-error");
-          }
-  */
-
-
-
-        /*const registro = {
+function imprimirJson(registroValido) {
+    if (registroValido) {
+        const registro = {
             nombre: document.getElementById("nombre")?.value.trim() || "",
             correo: document.getElementById("correo")?.value.trim() || "",
             telefono: document.getElementById("telefono")?.value.trim() || "",
@@ -72,23 +82,6 @@ document.getElementById("main-content").addEventListener("click", (e) => {
             equipo: Array.from(document.querySelectorAll('input[name="equipo[]"]:checked')).map(cb => cb.value),
             descripcion: document.getElementById("descripcion")?.value.trim() || ""
         };
-        console.log(JSON.stringify(registro, null, 2));*/
-    }
-});
-
-function obtenerElementos(idCampo, idError){
-    return{
-        campo: document.getElementById(idCampo),
-        error: document.getElementById(idError),
-    }
-}
-
-function validaInput(campo, error, mensaje) {
-    if (campo.value.trim() === "") {
-        error.textContent = mensaje;
-        campo.classList.add("input-error");
-    } else {
-        error.textContent = "";
-        campo.classList.remove("input-error");
+        console.log(JSON.stringify(registro, null, 2));
     }
 }
